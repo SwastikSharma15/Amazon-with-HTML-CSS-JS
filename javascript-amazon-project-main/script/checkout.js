40f163daafbadd3af91b1c92665ff134de1462db
Swastik <sharmaspeedx29@gmail.com>
2025-06-04 03:35:57 +0530

    regenerate html to keep site updated

--- javascript-amazon-project-main/script/checkout.js
+++ javascript-amazon-project-main/script/checkout.js
@@ -4,6 +4,7 @@ import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
 
+function renderOrderSummary() {
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => { 
@@ -196,6 +197,9 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
     const {productId, deliveryOptionId} = element.dataset;
 
     updateDeliveryOption(productId, deliveryOptionId);
+    renderOrderSummary();
   });
 });
-    
+
+}
+renderOrderSummary();
