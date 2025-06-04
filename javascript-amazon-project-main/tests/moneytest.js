655b9311b86a091eed189b4cc1da3ec32a30935e
Swastik <sharmaspeedx29@gmail.com>
2025-06-04 19:46:15 +0530

    added automated test for formatCurrency

---
+++ javascript-amazon-project-main/tests/moneytest.js
@@ -0,0 +1,27 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+console.log("Testing formatCurrency function...");
+
+console.log("convert cents to dollars");
+
+if (formatCurrency(1000) === "10.00") {
+    console.log("passed");
+} else {
+    console.error("failed");
+}
+
+console.log("Testing with 0 cents:");
+
+if (formatCurrency(0) === "0.00") {
+    console.log("passed");
+} else {
+    console.error("failed");
+}
+
+console.log("Testing with roundups:");
+
+if (formatCurrency(2000.5) === "20.01") {
+    console.log("passed");
+} else {
+    console.error("failed");
+}
