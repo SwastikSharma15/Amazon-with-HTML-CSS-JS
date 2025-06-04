d893c09c252b8ef4b3e43726e0c667670843742c
Swastik <sharmaspeedx29@gmail.com>
2025-06-04 18:40:26 +0530

    payment summary interactive render function pending

--- javascript-amazon-project-main/script/checkout/orderSummary.js
+++ javascript-amazon-project-main/script/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart,calculateCartQuantity,updateQuantity, updateDeliveryOption } from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 
 export function renderOrderSummary() {
 let cartSummaryHTML = '';
@@ -10,25 +10,22 @@ let cartSummaryHTML = '';
 cart.forEach((cartItem) => { 
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-        if (product.id === productId) {
-            matchingProduct = product;
-        }
-    });
+    const matchingProduct = getProduct(productId);
+    
     if (!matchingProduct) {
         return;
     }
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption = deliveryOptions.find(option => option.id === deliveryOptionId);
+    // let deliveryOption = deliveryOptions.find(option => option.id === deliveryOptionId);
 
-    if (!deliveryOption) {
-        console.warn(`No delivery option found for ID: ${deliveryOptionId}`);
-        return; // Skip rendering this cart item if delivery option is invalid
-    }
+    // if (!deliveryOption) {
+    //     console.warn(`No delivery option found for ID: ${deliveryOptionId}`);
+    //     return; // Skip rendering this cart item if delivery option is invalid
+    // }
+
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'day');
