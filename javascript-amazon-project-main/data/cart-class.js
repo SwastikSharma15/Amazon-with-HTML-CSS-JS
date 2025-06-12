5e2a5d01931944d95ce2b55ae2923224809c51fb
Swastik <sharmaspeedx29@gmail.com>
2025-06-12 19:33:56 +0530

    pending added too

--- javascript-amazon-project-main/data/cart-class.js
+++ javascript-amazon-project-main/data/cart-class.js
@@ -5,11 +5,11 @@ class Cart {
     constructor(localStorageKey) {
         this.#localStorageKey = localStorageKey;
 
-        this.loadFromStorage();
+        this.#loadFromStorage();
 
     }
 
-    loadFromStorage() {
+    #loadFromStorage() {
             this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
             if (!this.cartItems) {
