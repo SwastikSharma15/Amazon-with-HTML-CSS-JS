fd4722885ae0fd3ae2e2fcbcd94c342ad9b32585
Swastik <sharmaspeedx29@gmail.com>
2025-06-18 18:27:09 +0530

    used backend to load amazon home page and checkout page

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -78,8 +78,30 @@ export class Appliance extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === "appliance") {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails)
+    });
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
 
 
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -756,6 +778,7 @@ export const products = [
   }
    return new Product(productDetails)
 });
+*/
 
 
 /* const date = new Date();
